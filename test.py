99a5bdba63d80ab12f17c67dbe5841d2e3b341db
Ammar Ismail Baig <ammarismailbaig@gmail.com>
2026-04-10 19:45:57 -0500

    jksdbcaksjdhbc

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 n = int(input("enter an even number"))
 if n % 2 == 0:
     print("even number")
-elif n % 2 != 0:
-    print("odd number")
-    
 else:
-    print("Not a number")
+    print("odd number")
